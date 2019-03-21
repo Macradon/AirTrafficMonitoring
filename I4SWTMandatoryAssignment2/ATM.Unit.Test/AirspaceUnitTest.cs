2ecb53d7fade37e6f25afa644df78007134357d3
generousVoid <beratkaya@live.dk>
2019-03-21 13:09:37 +0100

    testcase for airspace

--- I4SWTMandatoryAssignment2/ATM.Unit.Test/AirspaceUnitTest.cs
+++ I4SWTMandatoryAssignment2/ATM.Unit.Test/AirspaceUnitTest.cs
@@ -4,26 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using I4SWTMandatoryAssignment2.Model;
 
-namespace ATM.Unit.Test
+
+namespace ATM.Test.Unit
+
 {
     [TestFixture]
-    public class AirspaceUnitTest
+    class AirspaceUnitTests
     {
-        private AirspaceUnitTest uut;
+        private Airspace uut;
+        private Track track;
 
         [SetUp]
-        public void Setup()
+        public void setup()
         {
-            uut = new AirspaceUnitTest();
+            uut = new Airspace();
         }
-
-        [TestCase]
-
-        public void isPositionOnTrack()
+        //Lav for x, y, og z
+        [TestCase(0, 40000, 10000)]
+        [TestCase(40000, 0, 15000)]
+        [TestCase(30000, 20000, 500)]
+        //Høj for x,y og z
+        [TestCase(80000, 40000, 10000)]
+        [TestCase(40000, 80000, 15000)]
+        [TestCase(30000, 20000, 20000)]
+        public void TrackInsideAirspace_ReturnTrue(int x, int y, int z)
         {
+            track = new Track()
+            {
+                Xcoor = x,
+                Ycoor = y,
+                Altitude = z
+            };
 
+            Assert.That(uut.positionTrack(track), Is.EqualTo(true));
         }
+            
+        
+    
 
 
     }
