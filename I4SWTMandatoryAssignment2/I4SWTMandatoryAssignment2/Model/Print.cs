a59c66baabb9e43f5b869ca5f237a363f63dff51
generousVoid <beratkaya@live.dk>
2019-03-21 11:46:01 +0100

    Print.cs tilføjet

---
+++ I4SWTMandatoryAssignment2/I4SWTMandatoryAssignment2/Model/Print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4SWTMandatoryAssignment2.Model
+{
+    public class Print
+    {
+        public void print(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public void Clear()
+        {
+          
+            Console.Clear();
+        }
+
+    }
+}
