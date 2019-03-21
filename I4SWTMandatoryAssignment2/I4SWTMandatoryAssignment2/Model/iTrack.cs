c71f2531366979955dd4166280c42b50be754933
MortenBech <mortenbech22@hotmail.com>
2019-03-21 11:32:42 +0100

    Added iTrack and iAirspace

---
+++ I4SWTMandatoryAssignment2/I4SWTMandatoryAssignment2/Model/iTrack.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4SWTMandatoryAssignment2.Model
+{
+    interface iTrack
+    {
+        string ToString();
+    }
+}
