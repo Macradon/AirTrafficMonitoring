0b249fa464e658665bc43e88d0f195f8448779e9
generousVoid <beratkaya@live.dk>
2019-03-21 12:40:06 +0100

    unit test tilføjet

---
+++ I4SWTMandatoryAssignment2/ATM.Unit.Test/AirspaceUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ATM.Unit.Test
+{
+    [TestFixture]
+    public class AirspaceUnitTest
+    {
+        private AirspaceUnitTest uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new AirspaceUnitTest();
+        }
+
+        [TestCase]
+
+        public void isPositionOnTrack()
+        {
+
+        }
+
+
+    }
+}
