70d53e17d92163084233d2a79ea1b496aa00ac10
generousVoid <beratkaya@live.dk>
2019-03-21 10:53:27 +0100

    Updated airspace

--- I4SWTMandatoryAssignment2/I4SWTMandatoryAssignment2/Model/Airspace.cs
+++ I4SWTMandatoryAssignment2/I4SWTMandatoryAssignment2/Model/Airspace.cs
@@ -4,21 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace I4SWTMandatoryAssignment2.Model
 {
     public class Airspace
     {
-        public int fhCornerX {get; set;}
-        public int fhCornerY { get; set; }
-        public int avCornerX { get; set; }
-        public int avCornery { get; set; }
+        public int rrCornerX {get; set;}
+        public int rrCornerY { get; set; }
+        public int llCornerX { get; set; }
+        public int llCornery { get; set; }
         public int minAlt { get; set; }
         public int maxAlt { get; set; }
 
         public Airspace()
         {
 
-            fhCornerX = 
+            llCornerX = 0;
+            llCornery = 0;
+
+            rrCornerX = 80000;
+            rrCornerY = 80000;
+
+            minAlt = 500;
+            maxAlt = 20000;
+
+
+
+        
 
         }
 
