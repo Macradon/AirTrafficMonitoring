caa6ac1955b99ebab82b549faccd2df326fd1dc4
generousVoid <beratkaya@live.dk>
2019-03-21 10:46:47 +0100

    igang med airspace.cs

---
+++ I4SWTMandatoryAssignment2/I4SWTMandatoryAssignment2/Model/Airspace.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4SWTMandatoryAssignment2.Model
+{
+    public class Airspace
+    {
+        public int fhCornerX {get; set;}
+        public int fhCornerY { get; set; }
+        public int avCornerX { get; set; }
+        public int avCornery { get; set; }
+        public int minAlt { get; set; }
+        public int maxAlt { get; set; }
+
+        public Airspace()
+        {
+
+            fhCornerX = 
+
+        }
+
+    }
+}
