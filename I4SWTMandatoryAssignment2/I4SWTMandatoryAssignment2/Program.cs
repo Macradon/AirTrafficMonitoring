af634531ae6d2b5fcf6a168804eb33e72ec65af1
Macradon <36033419+Macradon@users.noreply.github.com>
2019-03-22 12:04:15 +0100

    Rettet på nogle kommentarer der ikke hørte til her

--- I4SWTMandatoryAssignment2/I4SWTMandatoryAssignment2/Program.cs
+++ I4SWTMandatoryAssignment2/I4SWTMandatoryAssignment2/Program.cs
@@ -11,7 +11,7 @@ namespace I4SWTMandatoryAssignment2
     {
         static void Main(string[] args)
         {
-            //Lol test til at push fordi jeg ikke kan lide VisualStudioGit
+            
         }
     }
 }
