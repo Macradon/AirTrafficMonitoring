16fdb50e9f110518f56c7c7fab6ba3fb18a8d4bf
Fragarach95 <37145259+Fragarach95@users.noreply.github.com>
2019-04-19 12:11:24 +0200

    Opsættelse af klasser

---
+++ I4SWTMandatoryAssignment2_Genaflevering/AirTrafficMonitor/AirTrafficMonitor/Track.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficMonitor
+{
+    class Track
+    {
+        //attributes
+        private string _tag;
+        private int _x;
+        private int _y;
+        private int _alt;
+        private double _velocity;
+        private double _compass;
+        private DateTime _timestamp;
+    }
+}
