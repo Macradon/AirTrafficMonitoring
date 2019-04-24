566d6d16effc7c9dab30813b19d550be037bb806
MortenBech <mortenbech22@hotmail.com>
2019-04-24 11:16:00 +0200

    Added and Implemented decryptData

--- I4SWTMandatoryAssignment2_Genaflevering/AirTrafficMonitor/AirTrafficMonitor/Classes/Decrypting.cs
+++ I4SWTMandatoryAssignment2_Genaflevering/AirTrafficMonitor/AirTrafficMonitor/Classes/Decrypting.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AirTrafficMonitor.Classes;
 using TransponderReceiver;
+using System.Globalization;  //Used for DateTime
 
 namespace AirTrafficMonitor
 {
@@ -30,6 +31,39 @@ namespace AirTrafficMonitor
         public Decrypting()
         {
 
+        }
+
+        public void decryptData(string data)
+        {
+            CultureInfo provider = CultureInfo.InvariantCulture;
+
+            string[] splitter = new string[5];
+            splitter = data.Split(';');
+            int i = 0;
+
+            foreach (var variable in splitter)
+            {
+                splitter[i] = variable;
+                i++;
+            }
+
+            int tagLen = splitter[0].Length;
+            int xLen = splitter[1].Length;
+            int yLen = splitter[2].Length;
+            int altLen = splitter[3].Length;
+            int timeLen = splitter[4].Length;
+
+            int tagPos = 0;
+            int xPos = tagPos + tagLen + 1;
+            int yPos = xPos + xLen + 1;
+            int altPos = yPos + yLen + 1;
+            int timePos = altPos + altLen + 1;
+
+            _tag = data.Substring(tagPos, tagLen);
+            _x = Int32.Parse(data.Substring(xPos, xLen));
+            _y = Int32.Parse(data.Substring(yPos, yLen));
+            _alt = Int32.Parse(data.Substring(altPos, altLen));
+            _timestamp = DateTime.ParseExact(data.Substring(timePos, timeLen), "yyyyMMddHHmmssfff", provider);
         }
 
 
