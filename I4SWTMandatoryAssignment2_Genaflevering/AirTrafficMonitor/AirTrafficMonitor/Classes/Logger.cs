05a4167b403386f04994313389657d46851031c8
MortenBech <mortenbech22@hotmail.com>
2019-04-24 14:45:20 +0200

    Implemented Logger

--- I4SWTMandatoryAssignment2_Genaflevering/AirTrafficMonitor/AirTrafficMonitor/Classes/Logger.cs
+++ I4SWTMandatoryAssignment2_Genaflevering/AirTrafficMonitor/AirTrafficMonitor/Classes/Logger.cs
@@ -12,13 +12,24 @@ namespace AirTrafficMonitor
     public class Logger
     {
         iConflictDetection ConflictDetect = new ConflictDetection();
+        private string[] lines = new string[30];
+        private int lineNmb = 0;
 
-        private string[] lines { get; set; }
-        private int lineNmb { get; set; }
+        public string[] Lines { get; set; }
+        public int LineNmb { get; set; }
 
-        public void printToLog(string tag1, string tag2, DateTime tid)
+        public Logger()
         {
 
+        }
+
+        public void printToLog(string tag1, string tag2, DateTime time)
+        {
+            lines[lineNmb] = "Conflict between " + tag1 + " & " + tag2 + " at" +
+                             time.Hour + ":" + time.Minute + ":" + time.Second + "." + 
+                             time.Millisecond + " " + time.Day + "/" + time.Month + "-" + time.Year;
+            System.IO.File.WriteAllLines(@"C:\Users\Admin\Desktop\Logs.txt", lines);
+            lineNmb++;
         }
     }
 }
--- I4SWTMandatoryAssignment2_Genaflevering/AirTrafficMonitor/AirTrafficMonitor/Classes/Rendering.cs
+++ I4SWTMandatoryAssignment2_Genaflevering/AirTrafficMonitor/AirTrafficMonitor/Classes/Rendering.cs
@@ -17,9 +17,9 @@ namespace AirTrafficMonitor
            Console.WriteLine("Tag: " + track.Tag + ", X: " + track.Xcoor + ", Y: " + track.Ycoor + ", Altitude: " + track.Altitude + ", Velocity: " + track.Velocity + ", Course: " + track.Compass);
         }
 
-        public void printConflict(Track track)
+        public void printConflict(Track track1, Track track2)
         {
-            Console.WriteLine("Conflict between " + index[count.getTracks()].Tag + " & " + index[i].Tag +
+            Console.WriteLine("Conflict between " + track1.Tag + " & " + track2.Tag +
                         "   " + condition[set].TimeStamp.Day + "/" + condition[set].TimeStamp.Month + "-" + condition[set].TimeStamp.Year +
                         " " + condition[set].TimeStamp.Hour + ":" + condition[set].TimeStamp.Minute + ":" + condition[set].TimeStamp.Second +
                         "." + condition[set].TimeStamp.Millisecond);
