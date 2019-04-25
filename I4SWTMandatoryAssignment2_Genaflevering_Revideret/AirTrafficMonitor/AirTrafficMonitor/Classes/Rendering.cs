f2bdbe11d071382b0783f4fb41b0f3692ae48412
Fragarach95 <37145259+Fragarach95@users.noreply.github.com>
2019-04-25 21:07:49 +0200

    Tilføjet til Rendering samt rettet nogle interfaces til interfaces

--- I4SWTMandatoryAssignment2_Genaflevering_Revideret/AirTrafficMonitor/AirTrafficMonitor/Classes/Rendering.cs
+++ I4SWTMandatoryAssignment2_Genaflevering_Revideret/AirTrafficMonitor/AirTrafficMonitor/Classes/Rendering.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AirTrafficMonitor.Classes;
 
 namespace AirTrafficMonitor
 {
-    class Rendering
+    public class Rendering : IRendering
     {
+
+        public void printConflict(string Tag1, string Tag2, DateTime Time)
+        {
+            Console.WriteLine("Conflict between " + Tag1 + " & " + Tag2 + "   " +
+                              Time.Day + "/" + Time.Month + "-" + Time.Year + " " +
+                              Time.Hour + ":" + Time.Minute + ":" + Time.Second + "." + Time.Millisecond);
+        }
+
+        public void printTrack(Track track)
+        {
+
+            Console.WriteLine("Tag : " + track.Tag + ", X : " + track.Xcoor + ", Y : " + track.Ycoor + ", Altitude: " + track.Altitude + ", Velocity: " + track.Velocity + ", Course: " + track.Compass);
+        }
+
+        
     }
 }
