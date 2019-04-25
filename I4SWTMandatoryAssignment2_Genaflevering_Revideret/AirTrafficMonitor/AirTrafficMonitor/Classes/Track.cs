5d6f85f329a41baa688eded00930ece3a1789e3b
Macradon <36033419+Macradon@users.noreply.github.com>
2019-04-25 20:52:36 +0200

    Fjernet en metode i Track der ikke hørte til.

--- I4SWTMandatoryAssignment2_Genaflevering_Revideret/AirTrafficMonitor/AirTrafficMonitor/Classes/Track.cs
+++ I4SWTMandatoryAssignment2_Genaflevering_Revideret/AirTrafficMonitor/AirTrafficMonitor/Classes/Track.cs
@@ -43,17 +43,5 @@ namespace AirTrafficMonitor
             _compass = Compass;
             _timestamp = timestamp;
         }
-
-        //Formater function
-        public bool checkConflict(Track track1, Track track2)
-        {
-            if (Math.Sqrt(Math.Pow((track1.Xcoor - track2.Xcoor), 2) + Math.Pow((track1.Ycoor - track2.Ycoor), 2)) < 5000 &&
-                          (Math.Abs(track1.Altitude - track2.Altitude) < 300) && track1 != track2)
-                return true;
-            else
-                return false;
-
-        }
-
     }
 }
